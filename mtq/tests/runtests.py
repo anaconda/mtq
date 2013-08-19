48b8f03baf16ed934bf57b31f49ef498cbe6a1ba
srossross <srossross@gmail.com>
2013-08-19 12:59:58 -0700

    FIX: coverage

--- mtq/tests/runtests.py
+++ mtq/tests/runtests.py
@@ -8,7 +8,7 @@ from os.path import dirname
 
 def main():
     import coverage
-    cov = coverage.coverage()
+    cov = coverage.coverage(omit=['**/site-packages/**', '**/tests/**'])
     cov.start()
     
     loader = unittest.loader.TestLoader()
@@ -16,8 +16,8 @@ def main():
     runner = unittest.TextTestRunner()
     runner.run(tests) 
     cov.stop()
-    cov.report(omit=['**/site-packages/**', '**/tests/**'])
-    cov.html_report(omit=['**/site-packages/**', '**/tests/**'])
+    cov.save()
+    cov.report()
 
 if __name__ == '__main__':
     main()
