dd916520c9df406ef6b2272ec0ab54c066f47fc7
Francisco Capdevila <fjcapdevila@gmail.com>
2017-05-24 17:08:00 -0300

    It is really important to check tests results and return the proper exit status.

--- mtq/tests/runtests.py
+++ mtq/tests/runtests.py
@@ -4,8 +4,11 @@ Created on Aug 5, 2013
 @author: sean
 '''
 from __future__ import print_function
-import unittest
 from os.path import dirname
+
+import sys
+import unittest
+
 
 def main():
     import coverage
@@ -16,10 +19,14 @@ def main():
     loader = unittest.loader.TestLoader()
     tests = loader.discover(dirname(__file__))
     runner = unittest.TextTestRunner()
-    runner.run(tests) 
+    result = runner.run(tests)
     cov.stop()
     cov.save()
     cov.report()
+
+    # Exit code depends on tests result
+    import ipdb; ipdb.set_trace()
+    sys.exit(0 if result.wasSuccessful() else -1)
 
 if __name__ == '__main__':
     main()
